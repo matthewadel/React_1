62877d238a9d1f1e431304522eaedb65774196ad
matthewadel <matthewadel69@yahoo.com>
2025-07-08 19:27:50 +0300

    finish newsletter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import Analytics from "./components/analytics"
+import Analytics from "./components/Analytics"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Newsletter from "./components/Newsletter"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
   )
 }
